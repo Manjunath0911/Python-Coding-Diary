3de3b2806d925d1991118ed83033325703b99f52
Manjunath R Gowda <manjunthrgowda40@gmail.com>
2025-07-05 21:23:23 +0530

    day 1 new program is added succesfully- version 1.05.07.25

---
+++ pythonfile.py
@@ -0,0 +1,11 @@
+# this file is designed to upload algorithms
+
+# 1. Program to find even or not
+
+def even_number(num):
+    if num % 2 == 0:
+        return print(f"{num} is even number")
+    else:
+        return print(f"{num} is not even number")
+num  = int(input("enter the Numer : "))
+even_number(num)
